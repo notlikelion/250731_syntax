01067ee3ad32903182333b6284136cabe8fd7609
notlikelion <notlikelion@gmail.com>
2025-07-31 16:31:59 +0900

    제미나이 연결

---
+++ src/step2/GeminiChat.java
@@ -0,0 +1,13 @@
+package step2;
+
+public class GeminiChat {
+    // main
+    // 입출력
+    // 변수, 타입
+    public static void main(String[] args) {
+        // 입력 : 질문을 받아서
+        // Gemini와 통신하는 로직은요?
+        // 저한테 받아서 해야죠 뭐...
+        // 출력 : Gemini
+    }
+}
